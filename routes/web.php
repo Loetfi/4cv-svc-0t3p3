6f4ef7feac39d263bb976bc63f8240a1e05b873d
Tupay <lutfi@mhs.ubharajaya.ac.id>
2018-11-07 14:58:07 +0700

    add swagger documentation

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+use App\Helpers\Api;
+$router->get('/', function () use ($router) {
+    $data = $router->app->version();
+    return response()->json(Api::format(1, $data, 'Success'), 200);
+});
+
+$router->post('/send-otp', 'OTPController@index');
